85239e1edec73780baf360cc2967c95e3f2df229
louiskali <innocentlouis1@gmail.com>
2018-07-13 14:36:37 +0000

    premier commit

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+'name': 'Test_odoo',
+'version': '1.0',
+'sequence': 0,
+'category': 'test',
+'auteur': "odooTeam",
+
+'data': [
+    
+],
+'installable': True,
+'application': True,
+'auto_install': False,
+'qweb': [],
+}
